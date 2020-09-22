1dd4e8f00f6661bea90f790033978a2c34c2319d
Ray Hernandez <hernan43@gmail.com>
2020-09-22 19:35:24 -0400

    Setup form for testing

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,6 +1,6 @@
 class Game < ApplicationRecord
   belongs_to :user
-  has_many :save_files, dependant: :destroy
+  has_many :save_files, dependent: :destroy
 
   acts_as_taggable_on :platforms
 end
--- app/models/save_file.rb
+++ app/models/save_file.rb
@@ -1,2 +1,4 @@
 class SaveFile < ApplicationRecord
+  belongs_to :game
+  has_one_attached :sram
 end
